a8c338a7958aa9954384a95383f5efaa0a23adaa
PauloEverton-123 <pauloeverton8127@gmail.com.br>
2025-04-03 11:28:13 -0400

    atividade-01

---
+++ atividade-01.js
@@ -0,0 +1,14 @@
+let nome = 'Paulo Everton';
+let idade = 15;
+console.log ('Olá meu nome é', nome, 'e tenho', idade,'anos' );
+
+let n1 = 'tarefa';
+console.log (n1.toUpperCase());
+console.log (n1.toLowerCase());
+
+let num = 45;
+console.log (typeof num);
+
+let alt = 1.64;
+let peso = 98;
+console.log (peso/(alt*alt))
