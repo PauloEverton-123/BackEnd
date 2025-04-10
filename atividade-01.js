b731eaf06263c635101cddfbf463dea9f9419abd
PauloEverton-123 <pauloeverton8127@gmail.com.br>
2025-04-10 10:07:46 -0400

    correção

--- atividade-01.js
+++ atividade-01.js
@@ -1,14 +1,19 @@
+// Atividade 1
 let nome = 'Paulo Everton';
 let idade = 15;
-console.log ('Olá meu nome é', nome, 'e tenho', idade,'anos' );
+console.log('Olá meu nome é ' + nome + ' e tenho ' + idade + ' anos');
 
-let n1 = 'tarefa';
-console.log (n1.toUpperCase());
-console.log (n1.toLowerCase());
+// Atividade 2
+let n1 = 'tarEfa';
+console.log(n1.toUpperCase());
+console.log(n1.toLowerCase());
 
+// Atividade 3
 let num = 45;
-console.log (typeof num);
+console.log(typeof num);
 
-let alt = 1.64;
-let peso = 98;
-console.log (peso/(alt*alt))
+// Atividade 4
+let alt = 1.74;
+let peso = 54;
+let imc = (peso / (alt * alt)).toFixed(2);
+console.log('Seu imc é: ' + imc);
